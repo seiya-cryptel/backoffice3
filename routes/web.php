6e2dd4a72160f01dbfeedd9de21df3bec66e7654
seiya-cryptel <seiya@cryptel.co.jp>
2024-12-30 12:29:54 +0900

    Initial

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Middleware\Localization;
+
+Route::view('/', 'welcome');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__.'/auth.php';
